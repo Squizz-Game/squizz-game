9fa2190b1657a35487b9d2a7852c256b8bcec145
amina-seraoui <amina.seraoui@outlook.fr>
2022-03-21 15:00:30 +0100

    :hammer: page mon-compte

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -7,6 +7,44 @@ router.get('/deconnexion', (req, res) => {
     res.redirect('/connexion')
 })
 
+// Si connecté :
+router.get('/mon-compte', (req, res) => {
+    req.session.user_id = 2 // à retirer
+
+    if (req.session.user_id !== undefined) { // Si un utilisateur est connecté
+        console.log('utilisateur:', req.session.user_id)
+        User.get({...req.session}, (err, data) => {
+            if (!err) {
+                return res.render('mon-compte', { ...data })
+            } else {
+                res.redirect('/connexion')
+            }
+        })
+    } else {
+        // flash : vous n'êtes pas connecté
+        res.redirect('/connexion')
+    }
+})
+
+router.post('/mon-compte', (req, res) => {
+    if (req.session.user_id !== undefined) { // Si un utilisateur est connecté
+        User.update({...req.body, id_user: req.session.user_id}, (err, data) => {
+            if (err) {
+                console.log('error :', data)
+                // flash: error
+                return res.render('mon-compte', { ... req.body })
+            } else {
+                // flash: succès
+                console.log(data)
+                return res.redirect('/quizz')
+            }
+        })
+    } else {
+        // flash : vous n'êtes pas connecté
+        res.redirect('/connexion')
+    }
+})
+
 // Si non connecté :
 router.get('/inscription', (req, res) => {
     if (req.session.user_id === undefined) { // Si aucun utilisateur est connecté
--- back/models/user.js
+++ back/models/user.js
@@ -19,12 +19,76 @@ const User = {
             user_password = await bcrypt.hash(user_password, await bcrypt.genSalt(8))
             
             // Enregistrer en bdd
-            mysql.execute('INSERT INTO utilisateurs SET user_name=?, mail = ?, password = ?, id_avatar = ?',
+            mysql.execute('INSERT INTO utilisateurs SET user_name = ?, mail = ?, password = ?, id_avatar = ?',
             [user_name, user_mail, user_password, avatar_id],
             (err, rows) => {
                 if (err) return action(true, err)
                 return action(false, rows.insertId)
             })
+        })
+    },
+    update: ({avatar_id, user_mail, user_name, old_password, new_password, id_user}, action) => {
+        // Vérifier si tous les champs obligatoires sont renseignés
+        if (
+            id_user === (undefined || '') ||
+            avatar_id === (undefined || '') ||
+            user_mail === (undefined || '') ||
+            user_name === (undefined || '')
+        ) return action(true, 'Veuillez renseigner tous les champs requis.')
+
+        // Si un nouveau mot de passe est renseigné et que l'ancien est correct
+        if (new_password !== (undefined || '')) {
+            if (new_password.length < 6) {
+                return action(true, 'Nouveau mot de passe trop court (6 caractères min.).')
+            } else {
+                mysql.execute(
+                    'SELECT password FROM utilisateurs WHERE id_user = ?',
+                    [id_user],
+                    async (err, rows) => {
+                        // Vérifier si l'ancien mot de passe tapé correspond à l'ancien
+                        if (!err && bcrypt.compareSync(old_password, rows[0].password)) {
+                            // Hasher le nouveau password
+                            new_password = await bcrypt.hash(new_password, await bcrypt.genSalt(8))
+                            
+                            // Modifier les données avec le mot de passe
+                            mysql.execute(
+                                'UPDATE utilisateurs ' +
+                                'SET user_name = ?, mail = ?, password = ?, id_avatar = ? '+
+                                'WHERE id_user = ?',
+                                [user_name, user_mail, new_password, avatar_id, id_user],
+                                (err, rows) => {
+                                    if (err) return action(true, err)
+                                    return action(false, rows)
+                                }
+                            )
+                        } else {
+                            return action(true, 'L\'ancien mot de passe est incorrect.')
+                        }
+                    }
+                )
+            }
+        } else {
+            // Modifier les données sans le mot de passe
+            mysql.execute(
+                'UPDATE utilisateurs ' +
+                'SET user_name = ?, mail = ?, id_avatar = ? '+
+                'WHERE id_user = ?',
+                [user_name, user_mail, avatar_id, id_user],
+                (err, rows) => {
+                    if (err) return action(true, err)
+                    return action(false, rows)
+                }
+            )
+        }
+    },
+    get: ({user_id}, action) => {
+        if (user_id === (undefined || ''))
+            return action(true, 'Aucun utilisateur connecté')
+
+        mysql.execute('SELECT * FROM utilisateurs WHERE id_user = ?', [user_id], (err, rows) => {
+            if (err) return action(true, err)
+
+            return action(false, rows[0])
         })
     },
     check: ({user_name, user_password}, action) => {
