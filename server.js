4b2bf346a40c664c25bed9da64fe0015f4ff2367
Warrengers74 <warrengers.dev@gmail.com>
2022-03-22 14:01:28 +0100

    logique du quizz

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const user = require('./back/controllers/user')
-const quizz = require('./back/controllers/quizz')
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const user = require("./back/controllers/user");
+const quizz = require("./back/controllers/quizz");
+const api = require("./back/controllers/api");
 
-const app = express()
+const app = express();
 
 // Moteur de template
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 // Middlewares
-app.use('/assets', express.static('./public/assets'))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(session({
-    secret: 'my-secret-key',
+app.use("/assets", express.static("./public/assets"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  session({
+    secret: "my-secret-key",
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false } //if https : true
-}))
-app.use(require('./back/middlewares/flash'))
+    cookie: { secure: false }, //if https : true
+  })
+);
+app.use(require("./back/middlewares/flash"));
 
 // Controllers
-app.use('/', user)
-app.use('/quizz', quizz)
+app.use("/", user);
+app.use("/quizz", quizz);
+app.use("/api", api);
 
 // Home page
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.listen(9090, () => console.log('listening on http://localhost:9090/'))
+app.listen(9090, () => console.log("listening on http://localhost:9090/"));
