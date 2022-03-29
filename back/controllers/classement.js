5d5dd201d26c29a6191e47017c049de9b4df4574
Warrengers74 <warrengers.dev@gmail.com>
2022-03-29 16:56:23 +0200

    debug classement

--- back/controllers/classement.js
+++ back/controllers/classement.js
@@ -1,19 +1,20 @@
-const router = require('express').Router()
-const Score = require('../models/score')
-
+const router = require("express").Router();
+const Score = require("../models/score");
+const Quizz = require("../models/quizz");
 
 router.get("/:id_quizz", (req, res) => {
-    Score.getAll(req.params.id_quizz, (err, scores) => {
-        if (err) {
-            console.log(scores);
-        } else {
-            res.render("classement", {scores})
-        }
-        
-    })
-     
- })
+  Score.getAll(req.params.id_quizz, (err, scores) => {
+    if (err) {
+      console.log(scores);
+    } else {
+      Quizz.get(req.params.id_quizz, (err, quizz) => {
+        res.render("classement", {
+          scores,
+          quizz,
+        });
+      });
+    }
+  });
+});
 
-
-
-module.exports = router
+module.exports = router;
