a3cc2524ff1b3fb642d7f962573a40e4019c48f1
Warrengers74 <warrengers.dev@gmail.com>
2022-04-07 14:19:29 +0200

    petits détails

--- public/assets/js/classement.js
+++ public/assets/js/classement.js
@@ -6,10 +6,10 @@ usernames.forEach((username, i) => {
   username.addEventListener("click", () => {
     if (username.style.overflow == "hidden") {
       username.style.overflow = "visible";
-      ranking[i].style = "flex-wrap: wrap";
+        ranking[i].style = "flex-wrap: wrap";
     } else {
       username.style.overflow = "hidden";
-      ranking[i].style = "flex-wrap: nowrap";
+        ranking[i].style = "flex-wrap: nowrap";
     }
   });
 });
